53ead76e29a978bc109d111dcbdc167337084968
dextter1913 <Clau32usa@gmai.com>
2021-03-08 00:39:45 -0500

    actualziacion

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+    class persona
+    {
+        public $nombre = "Cristian";
+
+        public function hablar(){
+
+        }
+
+    }
+
+?>
+
+
+<?php 
+    $PPersona = new persona();
+    echo "$PPersona->nombre;"; 
+     
+?>
